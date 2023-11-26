38a82a7c1ae89472701d528a3dd3b8c0307ca06a
Alekosh <aakhosh00@gmail.com>
2023-11-26 08:28:30 +0500

    created a header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
